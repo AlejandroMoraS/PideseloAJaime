76c2ff5cd4f1d0b7719f32e51f940f5ec41c0ad9
Alejandro Mora <alex-mora95@hotmail.com>
2017-09-16 13:26:33 -0500

    Initial commit

---
+++ app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/ActivityMain.java
@@ -0,0 +1,13 @@
+package mx.iteso.jaimeapp.pideseloajaime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
