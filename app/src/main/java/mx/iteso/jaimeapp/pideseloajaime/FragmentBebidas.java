da7ac97395d71e84a465e0b12c1ba738311823f0
Alejandro Mora <alex-mora95@hotmail.com>
2017-10-08 18:20:31 -0500

    Fragments

---
+++ app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/FragmentBebidas.java
@@ -0,0 +1,29 @@
+package mx.iteso.jaimeapp.pideseloajaime;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentBebidas extends Fragment {
+
+
+    public FragmentBebidas() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bebidas, container, false);
+    }
+
+}
