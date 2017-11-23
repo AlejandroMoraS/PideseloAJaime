f802c5231d6e59f1f7c01fd8a4f09be9d0d55125
AlejandraSahagun <is696428@iteso.mx>
2017-11-22 23:53:49 -0600

    "Carrito de compras, insertar cantidad y borrar producto"

--- app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/gui/ActivityCarrito.java
+++ app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/gui/ActivityCarrito.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -21,16 +24,26 @@ public class ActivityCarrito extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito);
         HashMap<Product, Integer> products = Carrito.getInstance().getProducts();
-        TableLayout tl = (TableLayout)findViewById(R.id.activity_carrito_table);
-        for (Product p : products.keySet()) {
+        final TableLayout tl = (TableLayout)findViewById(R.id.activity_carrito_table);
+        for (final Product p : products.keySet()) {
             LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            TableRow rowView = (TableRow)inflater.inflate(R.layout.display_carrito, null);
+            final TableRow rowView = (TableRow)inflater.inflate(R.layout.display_carrito, null);
             TextView tv_product = (TextView)rowView.getChildAt(0);
             tv_product.setText(p.getProductName());
             TextView tv_cost = (TextView)rowView.getChildAt(1);
             tv_cost.setText("$" + p.getProductPrice());
+            EditText et_quantity = (EditText)rowView.getChildAt(2);
+            et_quantity.setText(products.get(p) + "");
+
             tl.addView(rowView);
+
+            Button btn = (Button)rowView.getChildAt(3);
+            btn.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    tl.removeView(rowView);
+                    Carrito.getInstance().removeProduct(p);
+                }
+            });
             /*
 
             TableRow tr = (TableRow)findViewById(R.id.activity_carrito_row);
@@ -39,5 +52,6 @@ public class ActivityCarrito extends AppCompatActivity {
             tl.addView(tr);
             */
         }
+
     }
 }
