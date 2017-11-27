a0b338e74477d5d99526588c7f4675eef7048421
alexmora95 <alex-mora95@hotmail.com>
2017-11-26 22:06:27 -0600

    Fin

--- app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/ActivityFinalizarCompra.java
+++ app/src/main/java/mx/iteso/jaimeapp/pideseloajaime/ActivityFinalizarCompra.java
@@ -1,7 +1,10 @@
 package mx.iteso.jaimeapp.pideseloajaime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class ActivityFinalizarCompra extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class ActivityFinalizarCompra extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalizar_compra);
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView tdate = (TextView) findViewById(R.id.id_date);
+                                long date = System.currentTimeMillis();
+                                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh-mm-ss a");
+                                String dateString = sdf.format(date);
+                                tdate.setText(dateString);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        t.start();
+
     }
 }
